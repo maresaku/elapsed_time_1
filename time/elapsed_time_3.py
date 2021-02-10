4de07c6f97e3955696fa4fbbe58e905c701d8d1e
maresaku <78681238+maresaku@users.noreply.github.com>
2021-02-10 20:27:21 +0900

    Update elapsed_time_3.py

--- time/elapsed_time_3.py
+++ time/elapsed_time_3.py
@@ -5,6 +5,7 @@ from datetime import datetime
 import pytz
 import time
 
+#
 Tokyo=pytz.timezone("Asia/Tokyo")
 start_time = datetime.now(Tokyo)
 
@@ -12,7 +13,7 @@ try:
 	while True:
 		time.sleep(60)
 		end_time = datetime.now(Tokyo)
-		elapsed_time = int(end_time.strftime("%H%M"))-int(start_time.strftime("%H%M"))
-		print(str(elapsed_time).zfill(4))
+		elapsed_time = str(int(end_time.strftime("%H%M"))-int(start_time.strftime("%H%M"))).zfill(4)
+		print(elapsed_time[:2]+str(':')+elapsed_time[2:])
 except KeyboardInterrupt:
   print('stop')
